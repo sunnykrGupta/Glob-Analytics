c145cfde088d738560c46b3a5097288cdf979cbc
Sunny K Gupta <sunnylautner4@gmail.com>
2015-04-27 15:33:49 +0530

    Batching collection and merging geo tagged tweets results scriptswith slight modification

--- Mongo/merging_batch_result.py
+++ Mongo/merging_batch_result.py
@@ -13,13 +13,13 @@ db_batch = client.batch_db
 db_main = client.main_db
 
 def merge_collection():
-    result = db_batch.pol_4_loc.find({}, {'_id' : False})
+    result = db_batch.col_4_loc.find({}, {'_id' : False})
     print "Result found :: %d" % (result.count())
     pol_result = []
     for r in result:
         pol_result.append(r)
-
-    pol_res = db_main.pol_geolocation.insert_many(pol_result)
+    #Change the Collection name
+    #pol_res = db_main.religion_geolocation.insert_many(pol_result)
     p_res = pol_res.inserted_ids
     print "Total docs appended are %d" % (len(p_res))
 

60ec35a8ae00e07b08f394f1050762ea8468d0b0
prat-hbk <sprateek63@yahoo.in>
2015-05-03 15:12:19 +0530

    all R scripts are done..

---
+++ Visualization/density_plot_topic.R
@@ -0,0 +1,62 @@
+# importing packages "Leaflet" and "maps"
+
+library(maps)
+library(leaflet)
+
+# function "density_plot_topic" is used to plot density wise circles on 
+# each topic from -------- 
+#   1) economy
+#   2) religion
+#   3) politics
+#   4) tourism
+#   5) overall
+
+density_plot_topic <- function(file_string){
+ 
+ argument_frame = data.frame( match = c("economy", "religion", "politics", "tourism", "overall"),
+                  files = c("~/work/Glob_Analytics/Visualization/csv/geo_economy.csv", "~/work/Glob_Analytics/Visualization/csv/geo_religion.csv", "~/work/Glob_Analytics/Visualization/csv/geo_politic.csv", "~/work/Glob_Analytics/Visualization/csv/geo_tourism.csv", "~/work/Glob_Analytics/Visualization/csv/world_tweets.csv")
+ )
+  
+ call_file <- ""
+ for(str in rev(argument_frame$match)){
+   
+   if(file_string == str) call_file <- as.character(argument_frame[match(c(str), argument_frame$match), c('files')])
+ }
+ 
+ df <- read.csv(file=call_file,head=TRUE,sep=",")
+ 
+
+ country_popup <- paste0("<strong>Country : </strong>", 
+                         df$country, 
+                        "<br><strong>Total : </strong>", 
+                        as.character(df$all)  
+ )
+
+ # dataframe for the radius assignment to the countries as per the tweets.
+ maxsize <- 15000
+ radii.df = data.frame( breaks = c(20, 50, 100, 500, 1000, 2000, 4000, 8000, maxsize), 
+ #                      color = c("darkblue", "lightseagreen", "green", 
+ #                                "lightblue", "purple", "orange", "red","darkred","blue"),
+                      radius = c(5, 10, 15, 20, 25, 30, 35, 40, 45)
+ )
+
+ # loop code for the assignment of radius column to the data frame
+ df$radius = rep(0, length(df$all))
+
+ for(bk in rev(radii.df$breaks)){
+  
+   df[df$all <= bk,]$radius <- 
+     radii.df[match(c(bk), radii.df$breaks), c('radius')]
+ }
+
+ #mapStates variable for map "world" to be coloured
+
+ mapStates = map("world", fill = TRUE, plot = FALSE)
+ leaflet(mapStates)%>% addTiles()%>%
+  addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)%>%
+  addCircleMarkers(lng = df$lng, lat = df$lat,  
+                   radius = df$radius, color = c('red'), 
+                   popup = country_popup) %>% setView(0.0,0.0, zoom =3)
+
+
+}
---
+++ Visualization/plot_per_tweets.R
@@ -0,0 +1,46 @@
+# this script plots the tweet's location topic wise and on the overall tweets
+# you have to call the function "plot_per_tweets" and pass the argument 
+# as string to the fuction :
+#   1) economy
+#   2) religion
+#   3) politics
+#   4) tourism
+#   5) overall
+
+library(maps)
+library(leaflet)
+
+plot_per_tweets <- function(file_string){
+  
+  argument_frame = data.frame( match = c("economy", "religion", "politics", "tourism", "overall"),
+                               files = c("economy_tweets.csv", "religion_tweets.csv", "politic_tweets.csv", "tourism_tweets.csv", "overall_plots")
+  )
+  
+  # loop for opening the file
+   call_file <- ""
+   for(str in rev(argument_frame$match)){
+    
+     if(file_string == str & str != "overall") call_file <- as.character(argument_frame[match(c(str), argument_frame$match), c('files')])
+   }
+  
+  data_frame <- data.frame()
+  if(file_string == "overall"){
+    eco <- read.csv(file="economy_tweets.csv",head=TRUE,sep=",")
+    rel <- read.csv(file="religion_tweets.csv",head=TRUE,sep=",")
+    pol <- read.csv(file="politic_tweets.csv",head=TRUE,sep=",")
+    tour <- read.csv(file="tourism_tweets.csv",head=TRUE,sep=",")
+    data_frame <- rbind(eco, rel, pol, tour) 
+  }
+  
+  df <- data.frame()
+  if(file_string=="overall") df <- data_frame
+  else df <- read.csv(file=call_file,head=TRUE,sep=",")
+  
+  #plotting the tweets on the world map
+  mapStates = map("world", fill = TRUE, plot = FALSE)
+  leaflet(mapStates)%>% addTiles()%>%
+    addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)%>%
+    addCircleMarkers(lng = df$lng, lat = df$lat,  
+                     radius = 1, color = c('red')) %>% setView(0.0,0.0, zoom =3)
+  
+}
